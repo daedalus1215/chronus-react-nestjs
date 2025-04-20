e0798f1b10925078140376f0c278c9dd7b6e5477
la la <13687739+daedalus1215@users.noreply.github.com>
2025-04-20 14:33:08 -0400

    infinite-scrolling: remove unused imports

--- frontend/src/pages/HomePage/hooks/useNotes.ts
+++ frontend/src/pages/HomePage/hooks/useNotes.ts
@@ -1,6 +1,4 @@
 import { useState, useEffect, useCallback } from 'react';
-import { useOnlineStatus } from '../../../hooks/useOnlineStatus';
-import { noteDB, Note } from '../../../services/indexedDB/noteDB';
 import api from '../../../api/axios.interceptor';
 
 type NoteResponse = {
