a911ecf8c87e1a3247008a036aba95f0694949cf
la la <13687739+daedalus1215@users.noreply.github.com>
2025-05-11 16:01:39 -0400

    add-check-list: create the entities and migration script. Then relocated notes request handlers

--- backend/src/notes/domain/entities/notes/checklist/checklistitem.entity.ts
+++ backend/src/notes/domain/entities/notes/checklist/checklistitem.entity.ts
@@ -1,17 +1,15 @@
-// import { Entity, PrimaryGeneratedColumn, ManyToMany, Column } from "typeorm";
-// import { Checklist } from "./checklist.entity";
+import { Entity, PrimaryGeneratedColumn, ManyToMany, Column } from "typeorm";
+import { Checklist } from "./checklist.entity";
 
 
-// @Entity("checklist_items")
-// export class ChecklistItem {
-//   @PrimaryGeneratedColumn()
-//   id: string;
+@Entity("checklist_items")
+export class ChecklistItem {
+  @PrimaryGeneratedColumn()
+  id: string;
 
-//   @Column("varchar")
-//   name: string;  // Add a name for the checklist item
+  @Column("varchar")
+  name: string; 
 
-//   @ManyToMany(() => Checklist, (checklist) => checklist.checklist_items)
-//   checklists: Checklist[];
-
-//   // Optional: If you still want the dates in the join table, they will live there, not here
-// }
+  @ManyToMany(() => Checklist, (checklist) => checklist.checklist_items)
+  checklists: Checklist[];
+}
--- backend/src/notes/notes.module.ts
+++ backend/src/notes/notes.module.ts
@@ -1,23 +1,25 @@
 import { Note } from "./domain/entities/notes/note.entity";
 import { Tag } from "./domain/entities/tag/tag.entity";
 import { TagNote } from "./domain/entities/tag/tag-note.entity";
-import { GetNoteNamesByUserIdAction } from "./apps/actions/get-note-names-by-userId.action";
+import { GetNoteNamesByUserIdAction } from "./apps/actions/notes/get-note-names-by-userId/get-note-names-by-userId.action";
 import { Memo } from "./domain/entities/notes/memo.entity";
-import { CreateNoteAction } from "./apps/actions/create-note-action/create-note.action";
+import { CreateNoteAction } from "./apps/actions/notes/create-note-action/create-note.action";
 import { CreateNoteTransactionScript } from "./domain/transaction-scripts/create-note.transaction.script";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Module } from "@nestjs/common";
 import { NoteMemoTagRepository } from "./infra/repositories/note-memo-tag.repository";
-import { GetNoteByIdAction } from './apps/actions/get-note-by-id-action/get-note-by-id.action';
+import { GetNoteByIdAction } from './apps/actions/notes/get-note-by-id-action/get-note-by-id.action';
 import { GetNoteByIdTransactionScript } from './domain/transaction-scripts/get-note-by-id.transaction.script';
-import { UpdateNoteAction } from "./apps/actions/update-note-action/update-note.action";
+import { UpdateNoteAction } from "./apps/actions/notes/update-note-action/update-note.action";
 import { UpdateNoteTransactionScript } from "./domain/transaction-scripts/update-note-TS/update-note.transaction.script";
 import { NoteDtoToEntityConverter } from "./domain/transaction-scripts/update-note-TS/note-dto-to-entity.converter";
 import { NoteAggregator } from './domain/aggregators/note.aggregator';
 import { UpdateNoteTimestampAction } from './apps/actions/update-note-timestamp.action';
+import { Checklist } from "./domain/entities/notes/checklist/checklist.entity";
+import { ChecklistItem } from "./domain/entities/notes/checklist/checklistitem.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note, Memo, Tag, TagNote])],
+  imports: [TypeOrmModule.forFeature([Note, Memo, Tag, TagNote, Checklist, ChecklistItem])],
   controllers: [
     GetNoteNamesByUserIdAction,
     CreateNoteAction,
