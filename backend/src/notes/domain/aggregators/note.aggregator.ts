5bfb79bbb34207dec65bd849ea85b2bc7c869ca9
la la <13687739+daedalus1215@users.noreply.github.com>
2025-05-13 21:43:23 -0400

    add-check-list: update note aggregator to use userId

--- backend/src/notes/domain/aggregators/note.aggregator.ts
+++ backend/src/notes/domain/aggregators/note.aggregator.ts
@@ -14,13 +14,13 @@ export class NoteAggregator {
     private readonly noteRepository: NoteMemoTagRepository
   ) {}
 
-  async exists(noteId: number): Promise<boolean> {
-    const note = await this.noteRepository.findById(noteId);
+  async exists(noteId: number, userId: string): Promise<boolean> {
+    const note = await this.noteRepository.findById(noteId, userId);
     return !!note;
   }
 
-  async getReference(noteId: number): Promise<NoteReference | null> {
-    const note = await this.noteRepository.findById(noteId);
+  async getReference(noteId: number, userId: string): Promise<NoteReference | null> {
+    const note = await this.noteRepository.findById(noteId, userId);
     if (!note) return null;
 
     return {
@@ -32,15 +32,15 @@ export class NoteAggregator {
 
   async belongsToUser(params:{noteId: number, user:{id: number}}) {
     //@TODO: refactor to use transaction script
-    const note = await this.noteRepository.findById(params.noteId);
+    const note = await this.noteRepository.findById(params.noteId, params.user.id.toString());
 
     if (parseInt(note.userId) !== params.user.id) {
       throw new ForbiddenException('Not authorized to access this note');
     }
   }
 
-  async isArchived(noteId: number): Promise<boolean> {
-    const note = await this.noteRepository.findById(noteId);
+  async isArchived(noteId: number, userId: string): Promise<boolean> {
+    const note = await this.noteRepository.findById(noteId, userId);
     return note?.archivedDate !== null;
   }
 } 
