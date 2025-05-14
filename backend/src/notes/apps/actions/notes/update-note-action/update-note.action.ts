e4c6271a35a6ef526f4a21b497f427cf754d0ddf
la la <13687739+daedalus1215@users.noreply.github.com>
2025-05-13 21:42:44 -0400

    add-check-list: update upodate note action with auth decorator to get user id

--- backend/src/notes/apps/actions/notes/update-note-action/update-note.action.ts
+++ backend/src/notes/apps/actions/notes/update-note-action/update-note.action.ts
@@ -4,6 +4,8 @@ import { UpdateNoteDto } from '../../../dtos/requests/update-note.dto';
 import { NoteResponseDto } from '../../../dtos/responses/note.response.dto';
 import { ProtectedAction } from 'src/time-tracks/apps/decorators/protected-action.decorator';
 import { UpdateNoteSwagger } from './update-note.swagger';
+import { GetAuthUser } from 'src/auth/app/decorators/get-auth-user.decorator';
+import { AuthUser } from 'src/auth/app/decorators/get-auth-user.decorator';
 
 @Controller('notes')
 export class UpdateNoteAction {
@@ -15,8 +17,9 @@ export class UpdateNoteAction {
   @ProtectedAction(UpdateNoteSwagger)
   async apply(
     @Param('id') id: string,
-    @Body() updateNoteDto: UpdateNoteDto,
+    @Body() updateNoteDto: UpdateNoteDto, 
+    @GetAuthUser() authUser: AuthUser
   ): Promise<NoteResponseDto> {
-    return await this.updateNoteTransactionScript.apply(parseInt(id, 10), updateNoteDto);
+    return await this.updateNoteTransactionScript.apply(parseInt(id, 10), updateNoteDto, authUser.userId);
   }
 } 
