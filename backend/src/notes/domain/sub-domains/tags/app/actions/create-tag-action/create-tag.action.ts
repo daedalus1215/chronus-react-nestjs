be826887f9c92cada2d6161c5bfd8841623a64f8
Daedalus <13687739+daedalus1215@users.noreply.github.com>
2025-06-30 21:58:42 -0400

    tags: update create-tag action to use auth

---
+++ backend/src/notes/domain/sub-domains/tags/app/actions/create-tag-action/create-tag.action.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body } from "@nestjs/common";
+import { CreateTagTransactionScript } from "../../../domain/transaction-scripts/create-tag.transaction.script";
+import { CreateTagDto } from "../../dtos/create-tag.dto";
+import { GetAuthUser } from "src/auth/app/decorators/get-auth-user.decorator";
+import { ProtectedAction } from "src/time-tracks/apps/decorators/protected-action.decorator";
+import { CreateTagSwagger } from "./create-tag.swagger";
+
+@Controller("tags")
+export class CreateTagAction {
+  constructor(private readonly createTagTS: CreateTagTransactionScript) {}
+
+  @Post()
+  @ProtectedAction(CreateTagSwagger)
+  async createTag(
+    @Body() createTagDto: CreateTagDto,
+    @GetAuthUser("userId") userId: string
+  ) {
+    return this.createTagTS.apply({ ...createTagDto, userId });
+  }
+}
